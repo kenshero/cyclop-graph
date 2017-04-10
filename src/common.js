7ba934bc16caf2cb5aaacd32471f53d7adcd90b5
kenshero <kenshero.k@gmail.com>
2017-04-11 01:21:08 +0700

    fixed for style guide

--- src/common.js
+++ src/common.js
@@ -1,69 +1,63 @@
 export const mapDocWithVariables = (doc, variables) => {
-  const varsFromDocument = getVariblesFromDocument(doc);
-  const variablesSet = matchVar(varsFromDocument, variables);
+  const varsFromDocument = getVariblesFromDocument(doc)
+  const variablesSet = matchVar(varsFromDocument, variables)
   const infoDoc = replaceVariablestoDoc(doc, variablesSet)
-  return infoDoc;
+  return infoDoc
 }
 
 const getVariblesFromDocument = (doc) => {
-  const regExp = /\(([^)]+)\)/g;
-  const matches = doc.match(regExp);
-  let varsFromDocument = [];
+  const regExp = /\(([^)]+)\)/g
+  const matches = doc.match(regExp)
+  const varsFromDocument = []
   for (let i = 0; i < matches.length; i++) {
-      let str = matches[i].substring(1, matches[i].length - 1);
-      varsFromDocument.push(str);
+    const str = matches[i].substring(1, matches[i].length - 1)
+    varsFromDocument.push(str)
   }
-  return varsFromDocument;
+  return varsFromDocument
 }
 
 const matchVar = (varsFromDocument, variables) => {
-  let newVars;
-  if(varsFromDocument.length != variables.length) {
+  let newVars
+  if (varsFromDocument.length !== variables.length) {
     throw 'Document and Variables does not match.'
-  } else if(isMatchVars(varsFromDocument, variables)){
-      newVars = processMapVars(varsFromDocument, variables)
+  } else if (isMatchVars(varsFromDocument, variables)) {
+    newVars = processMapVars(varsFromDocument, variables)
   }
-  return newVars;
+  return newVars
 }
 
 const replaceVariablestoDoc = (doc, variablesSet) => {
-  let infoDoc = doc;
-  const varsFromDocument = getVariblesFromDocument(doc);
-  for(let i = 0; i < variablesSet.length; i++) {
+  let infoDoc = doc
+  const varsFromDocument = getVariblesFromDocument(doc)
+  for (let i = 0; i < variablesSet.length; i++) {
     infoDoc = infoDoc.replace(varsFromDocument[i], variablesSet[i])
   }
-  return infoDoc;
+  return infoDoc
 }
 
-
-
-
-
-
-
 const processMapVars = (varsFromDocument, variables) => {
-  const newVars = varsFromDocument.map( (varFromDoc, index) => {
-    const keys = Object.keys(variables[index]);
-    let replaceVar = varFromDoc;
-    for(let i = 0; i < keys.length; i++) {
+  const newVars = varsFromDocument.map((varFromDoc, index) => {
+    const keys = Object.keys(variables[index])
+    let replaceVar = varFromDoc
+    for (let i = 0; i < keys.length; i++) {
       replaceVar = replaceVar.replace(`$${keys[i]}`, variables[index][keys[i]])
     }
-    return replaceVar;
-   });
+    return replaceVar
+  });
   return newVars;
 }
 
 
 const isMatchVars = (varsFromDocument, variables) => {
-  for( let i = 0; i < varsFromDocument.length; i++) {
-    const splitVarsFromDocument = varsFromDocument[i].split(",");
-    const vars = Object.keys(variables[i]);
-    if(splitVarsFromDocument.length != vars.length) {
+  for (let i = 0; i < varsFromDocument.length; i++) {
+    const splitVarsFromDocument = varsFromDocument[i].split(',')
+    const vars = Object.keys(variables[i])
+    if (splitVarsFromDocument.length !== vars.length) {
       throw 'Variable dose not match.'
     }
-    for(let n = 0; n < vars.length; n++) {
+    for (let n = 0; n < vars.length; n++) {
       const searchWord = splitVarsFromDocument[n].includes(`$${vars[n]}`)
-      if(!searchWord) {
+      if (!searchWord) {
         throw `'${vars[n]}' dose not match.`
       }
     }
--- src/cyclop.js
+++ src/cyclop.js
@@ -1,8 +1,8 @@
-import 'whatwg-fetch';
+import 'whatwg-fetch'
 import { mapDocWithVariables } from './common'
 
 const fetchData = async (doc, url) => {
-  let queryData;
+  let queryData
   try {
     const response = await fetch(url, {
       method: 'POST',
@@ -10,39 +10,26 @@ const fetchData = async (doc, url) => {
         'Content-Type': 'application/graphql'
       },
       body: doc
-    });
-    queryData = await response.json();
-  } catch(errors) {
-    console.error(errors);
+    })
+    queryData = await response.json()
+  } catch (errors) {
+    console.error(errors)
   }
-  console.log(url);
-  return queryData;
+  return queryData
 }
 
-export function ConnectionGraphql(network){
-  const {url, headers} = network;
-  this.url = url;
-  this.headers = headers;
-  this.query = async function(doc, variables = null) {
-    let queryData;
-    if(variables) {
+export function ConnectionGraphql(network) {
+  const { url, headers } = network
+  this.url = url
+  this.headers = headers
+  this.query = async function (doc, variables = null) {
+    let queryData
+    if (variables) {
       const infoDoc = mapDocWithVariables(doc, variables)
       queryData = await fetchData(infoDoc, this.url)
     } else {
       queryData = await fetchData(doc, this.url)
     }
-    return queryData;
+    return queryData
   } // query
 }
-
-export const sum = (a, b) => {
-  const sumTotal = a + b;
-  return sumTotal;
-};
-
-export const getPost = () => {
-  fetch('https://jsonplaceholder.typicode.com/posts').then((response) => {
-    console.log(response);
-  });
-};
-
